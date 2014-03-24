2d2c925875eacb9de210b8fe0c2b46d00318782e
Paolo Ardoino <paolo.ardoino@gmail.com>
2014-03-24 16:36:58 +0100

    General improvements

--- examples/test_client.js
+++ examples/test_client.js
@@ -3,8 +3,15 @@ var Client = require('./../index').Client;
 var client = new Client('tcp://localhost:5555');
 client.start();
 
-client.request("echo", { foo1: 'bar1' }, function() {
-	console.log("PARTIAL");   
-}, function() {
-	console.log("FINAL");   
-});
+var rcnt = 0;
+
+for (var i = 0; i < 1; i++) {
+	(function(i) {
+		client.request("echo", { spot: false }, function() {
+			console.log("PARTIAL", i);  
+		}, function() {
+			rcnt++;
+			console.log("FINAL", i, rcnt);
+		});
+	})(i);
+}
--- examples/test_worker.js
+++ examples/test_worker.js
@@ -1,39 +1,56 @@
+var cluster = require('cluster');
 var Worker = require('./../index').Worker;
 
-var worker = new Worker('tcp://localhost:5555', 'echo');
-worker.start();
-
-
-function go(inp, rep) {
-
-	console.log(inp);
-
-	function partial() {
-		if (!rep.active()) {
-			final();
-			return;
-		}
-		rep.write("prova-" + rep.rid + '-' + (new Date().getTime()));
+if (cluster.isMaster) {
+	for (var i = 0; i < 4; i++) {
+		cluster.fork();
 	}
+	cluster.on('exit', function(worker, code, signal) {
+		console.log('worker ' + worker.process.pid + ' died');
+	});
+} else {
 
-	function final() {
-		clearInterval(rtmo);
-		if (!rep.active()) {
-			return;
+	(function() {
+		var worker = new Worker('tcp://localhost:5555', 'echo');
+			worker.start();
+
+		function go(inp, rep) {
+
+			console.log(inp);
+
+			function partial() {
+				if (!rep.active()) {
+					final();
+					return;
+				}
+				rep.write("prova-" + rep.rid + '-' + (new Date().getTime()));
+			}
+
+			function final() {
+				clearInterval(rtmo);
+				if (!rep.active()) {
+					return;
+				}
+				rep.end("prova-end");
+			}
+
+			if (inp.spot) {
+				setTimeout(function() {
+					final();
+				}, 1000);
+			} else {
+				var rtmo = setInterval(function() {
+					partial();	
+				}, 250);
+
+				setTimeout(function() {
+					final();
+				}, 100000);
+			}
 		}
-		rep.end("prova-end");
-	}
 
-	var rtmo = setInterval(function() {
-		partial();	
-	}, 250);
-
-	setTimeout(function() {
-		final();
-	}, 100000);
+		worker.on('request', function(inp, rep) {
+			go(inp, rep);	
+		});
+	})();
 }
-
-worker.on('request', function(inp, rep) {
-	go(inp, rep);	
-});
-
