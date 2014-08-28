e6cd498f8ba54adca2d6969f8248f9c8fb6e7576
Paolo Ardoino <paolo.ardoino@gmail.com>
2014-08-28 11:59:33 +0200

    Minor changes. package.json updated

--- examples/perf.js
+++ examples/perf.js
@@ -15,38 +15,40 @@ if (cluster.isMaster) {
 	});
 } else {
 	var workerID = cluster.worker.workerID;
-	if (workerID == 1) {
-		var broker = new omdp.Broker('tcp://*:55559');
-		broker.start(function() {
-			console.log("BROKER started");
-		});
-
-	} else if (workerID == 2) {
-		var worker = new omdp.Worker('tcp://localhost:55559', 'echo');
-		worker.on('request', function(inp, res) {
-			console.log("WORKER req received");
-			console.log("\tsending " + probes + " probes");
-			for (var i = 0; i < probes; i++) {
-				res.write(inp);
-			}
-			res.end(inp + 'FINAL'); 
-		});
-		worker.start();
-
-	} else if (workerID == 3) {
-		var client = new omdp.Client('tcp://localhost:55559');
+	switch (+workerID) {
+		case 1:
+			var broker = new omdp.Broker('tcp://*:55559');
+			broker.start(function() {
+				console.log("BROKER started");
+			});
+		break;
+		case 2:
+			var worker = new omdp.Worker('tcp://localhost:55559', 'echo');
+			worker.on('request', function(inp, res) {
+				console.log("WORKER req received");
+				console.log("\tsending " + probes + " probes");
+				for (var i = 0; i < probes; i++) {
+					res.write(inp);
+				}
+				res.end(inp + 'FINAL');
+			});
+			worker.start();
+		break;
+		case 3:
+			var client = new omdp.Client('tcp://localhost:55559');
 			client.start();
 
-		var d1 = new Date();
-		client.request(
-			'echo', chunk,
-			function(data) {}, 
-			function(err, data) {
-				var dts = (new Date() - d1);
-				console.log("CLIENT GOT answer", dts + " milliseconds. " + (probes / (dts / 1000)).toFixed(2) + " requests/sec.");
-				client.stop();
-				process.exit(-1);
-			}
-		);
+			var d1 = new Date();
+			client.request(
+				'echo', chunk,
+				function(data) {},
+				function(err, data) {
+					var dts = (new Date() - d1);
+					console.log("CLIENT GOT answer", dts + " milliseconds. " + (probes / (dts / 1000)).toFixed(2) + " requests/sec.");
+					client.stop();
+					process.exit(-1);
+				}
+			);
+		break;
 	}
 }
