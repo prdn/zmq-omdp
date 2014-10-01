cabbcb94b0cf794fa4d98f39c0a2d53d1c4583d6
Paolo Ardoino <paolo.ardoino@gmail.com>
2014-10-01 16:07:44 +0200

    Error handling semplification. More tests

--- test/index.js
+++ test/index.js
@@ -3,30 +3,28 @@ var omdp = require('../');
 
 var location = 'inproc://#1';
 
-test('echo server (partial/final)', function(t) {
-	t.plan(104);
+test('test server (partial/final)', function(t) {
+	t.plan(8);
 
 	var chunk = 'foo';
 
-	var worker = new omdp.Worker(location, 'echo');
-	worker.on('request', function (inp, res) {
-		inp = String(inp);
-		t.equal(inp, chunk, 'Worker input is equal to expected');
-		for (var i = 0; i < 100; i++) {
+	var worker = new omdp.Worker(location, 'test');
+	worker.on('request', function(inp, res) {
+		t.equal(inp, chunk, 'Worker input');
+		for (var i = 0; i < 5; i++) {
 			res.write(inp + i);
 		}
 		res.end(inp + 'FINAL' + (++i)); 
 	});
-
-	var client = new omdp.Client(location);
-
 	worker.start();
-	client.start();
 
 	var broker = new omdp.Broker(location);
 	broker.start(function() {
-		t.pass('Broker callback was called');
+		t.pass('Broker callback');
 	});
+	
+	var client = new omdp.Client(location);
+	client.start();
 
 	function stop() {
 		worker.stop();
@@ -34,19 +32,83 @@ test('echo server (partial/final)', function(t) {
 		broker.stop();
 	}
 
-	(function() {
-		var repIx = 0;
-		var d1 = new Date();
-		client.request(
-			'echo', chunk,
-			function(data) {
-				t.equal(String(data), chunk + (repIx++), 'Worker output PARTIAL (' + (repIx - 1) + ')');
-			}, 
-			function(err, data) {
-				t.equal(String(data), chunk + 'FINAL' + (++repIx), 'Worker output FINAL (' + (repIx - 1) + ')');
-				t.equal(true, true, (new Date() - d1) + ' milliseconds');
-				stop();
-			}
-		);
-	})();
+	var repIx = 0;
+	client.request(
+		'test', chunk,
+		function(data) {
+			t.equal(data, chunk + (repIx++), 'Partial msg matches');
+		}, 
+		function(err, data) {
+			t.equal(data, chunk + 'FINAL' + (++repIx), 'Final msg matches');
+			stop();
+		}
+	);
+});
+
+test('test server (final/error)', function(t) {
+	t.plan(2);
+
+	var chunk = 'SOMETHING_FAILED';
+
+	var worker = new omdp.Worker(location, 'test');
+	worker.on('request', function(inp, res) {
+		res.error(chunk); 
+	});
+	worker.start();
+
+	var broker = new omdp.Broker(location);
+	broker.start(function() {});
+	
+	var client = new omdp.Client(location);
+	client.start();
+
+	function stop() {
+		worker.stop();
+		client.stop();
+		broker.stop();
+	}
+
+	client.request(
+		'test', 'foo',
+		function(data) {}, 
+		function(err, data) {
+			t.equal(err, chunk, 'Error msg matches');
+			t.equal(data, null, 'Data is not defined');
+			stop();
+		}
+	);
+});
+
+test('test server (JSON/final)', function(t) {
+	t.plan(2);
+
+	var chunk = { foo: 'bar' };
+
+	var worker = new omdp.JSONWorker(location, 'test');
+	worker.on('request', function(inp, res) {
+		res.end(chunk); 
+	});
+	worker.start();
+
+	var broker = new omdp.Broker(location);
+	broker.start(function() {});
+	
+	var client = new omdp.JSONClient(location);
+	client.start();
+
+	function stop() {
+		worker.stop();
+		client.stop();
+		broker.stop();
+	}
+
+	client.request(
+		'test', 'foo',
+		function(data) {}, 
+		function(err, data) {
+			t.equal(err, null, 'Error is null');
+			t.deepEqual(data, chunk, 'Final message matches');
+			stop();
+		}
+	);
 });
