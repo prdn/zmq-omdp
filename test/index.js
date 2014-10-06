a119ed97126294bc58eb228146a8cda946a478be
Paolo Ardoino <paolo.ardoino@gmail.com>
2014-10-06 09:43:30 +0100

    Switched to using mocha/chai

--- test/index.js
+++ test/index.js
@@ -1,117 +1,139 @@
-var test = require('tape');
 var omdp = require('../');
+var chai = require('chai');
 
 var location = 'inproc://#1';
 
-var broker;
+describe('BASE', function() {
+	var broker = new omdp.Broker(location)
+	broker.start(function() {});
 
-test('Broker startup', function(t) {
-	t.plan(1);
-	broker = new omdp.Broker(location);
-	broker.start(function() {
-		t.pass('Broker callback');
-	});
-});
+	it('Client partial/final request (stream)', function(done) {
+		var chunk = 'foo';
 
-test('partials/final (callback)', function(t) {
-	t.plan(7);
+		var worker = new omdp.Worker(location, 'test');
 
-	var chunk = 'foo';
+		worker.on('request', function(inp, res) {
+			for (var i = 0; i < 5; i++) {
+				res.write(inp + i);
+			}
+			res.end(inp + (i));
+		});
 
-	var worker = new omdp.Worker(location, 'test');
-	worker.on('request', function(inp, res) {
-		t.equal(inp, chunk, 'Worker input');
-		for (var i = 0; i < 5; i++) {
-			res.write(inp + i);
-		}
-		res.end(inp + 'FINAL' + (++i)); 
-	});
-	worker.start();
+		worker.start();
 
-	var client = new omdp.Client(location);
-	client.start();
+		var client = new omdp.Client(location);
+		client.start();
 
-	function stop() {
-		worker.stop();
-		client.stop();
-	}
+		var repIx = 0;
 
-	var repIx = 0;
-	client.request(
-		'test', chunk,
-		function(err, data) {
-			t.equal(data, chunk + (repIx++), 'Partial msg matches');
-		}, 
-		function(err, data) {
-			t.equal(data, chunk + 'FINAL' + (++repIx), 'Final msg matches');
+		client.request(
+			'test', chunk
+		).on('data', function(data) {
+			chai.assert.equal(data, String(chunk + (repIx++)));
+		}).on('end', function() {
 			stop();
+		});
+
+		function stop() {
+			worker.stop();
+			client.stop();
+			done();
 		}
-	);
-});
-
-test('final+error (callback)', function(t) {
-	t.plan(2);
-
-	var chunk = 'SOMETHING_FAILED';
-
-	var worker = new omdp.Worker(location, 'test');
-	worker.on('request', function(inp, res) {
-		res.error(chunk); 
 	});
-	worker.start();
 
-	var client = new omdp.Client(location);
-	client.start();
+	it('Client partial/final request (callback)', function(done) {
+		var chunk = 'foo';
 
-	function stop() {
-		worker.stop();
-		client.stop();
-	}
+		var worker = new omdp.Worker(location, 'test');
 
-	client.request(
-		'test', 'foo',
-		function(err, data) {},
-		function(err, data) {
-			t.equal(err, chunk, 'Error msg matches');
-			t.equal(data, null, 'Data is not defined');
-			stop();
+		worker.on('request', function(inp, res) {
+			for (var i = 0; i < 5; i++) {
+				res.write(inp + i);
+			}
+			res.end(inp + 'FINAL' + (++i));
+		});
+
+		worker.start();
+
+		var client = new omdp.Client(location);
+		client.start();
+
+		var repIx = 0;
+
+		client.request(
+			'test', chunk,
+			function(err, data) {
+				chai.assert.equal(data, chunk + (repIx++));
+			}, 
+			function(err, data) {
+				chai.assert.equal(data, chunk + 'FINAL' + (++repIx));
+				stop();
+			}
+		);
+
+		function stop() {
+			worker.stop();
+			client.stop();
+			done();
 		}
-	);
-});
-
-test('JSON final (callback)', function(t) {
-	t.plan(2);
-
-	var chunk = { foo: 'bar' };
-
-	var worker = new omdp.JSONWorker(location, 'test');
-	worker.on('request', function(inp, res) {
-		res.end(chunk); 
 	});
-	worker.start();
 
-	var client = new omdp.JSONClient(location);
-	client.start();
+	it('Client error request (stream)', function(done) {
+		var chunk = 'SOMETHING_FAILED';
 
-	function stop() {
-		worker.stop();
-		client.stop();
-	}
+		var worker = new omdp.Worker(location, 'test');
 
-	client.request(
-		'test', 'foo',
-		function(err, data) {},
-		function(err, data) {
-			t.equal(err, null, 'Error is null');
-			t.deepEqual(data, chunk, 'Final message matches');
+		worker.on('request', function(inp, res) {
+			res.error(chunk);
+		});
+
+		worker.start();
+
+		var client = new omdp.Client(location);
+		client.start();
+
+		client.request(
+			'test', chunk
+		).on('error', function(err) {
+			chai.assert.equal(err, chunk);
 			stop();
+		});
+
+		function stop() {
+			worker.stop();
+			client.stop();
+			done();
 		}
-	);
-});
+	});
 
-test('Broker stop', function(t) {
-	t.plan(1);
+	it('Client error request (callback)', function(done) {
+		var chunk = 'SOMETHING_FAILED';
 
-	broker.stop();
-	t.pass('Broker exited');
+		var worker = new omdp.Worker(location, 'test');
+
+		worker.on('request', function(inp, res) {
+			res.error(chunk);
+		});
+
+		worker.start();
+
+		var client = new omdp.Client(location);
+		client.start();
+
+		client.request(
+			'test', chunk,
+			undefined,
+			function(err, data) {
+				chai.assert.equal(err, chunk);
+				chai.assert.equal(data, null);
+				stop();
+			}
+		);
+
+		function stop() {
+			worker.stop();
+			client.stop();
+			done();
+		}
+	});
 });
